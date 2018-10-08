e8aee280eb82143ecd5e83bca9710a58bdda2eff
pjchender <pjchender@gmail.com>
2018-10-08 20:28:18 +0800

    Setup initial express-http-proxy

--- src/index.server.js
+++ src/index.server.js
@@ -1,11 +1,24 @@
 // Entry point for webpack.server.js
 import express from 'express';
 import { matchRoutes } from 'react-router-config';
+import proxy from 'express-http-proxy';
 import renderer from '@/server_helpers/renderer';
 import createStore from '@/server_helpers/createStore';
 import Routes from '@/client/Routes';
 
 const app = express();
+
+// 任何想要存取 /api 路由的請求，都會被轉導到另一個網址
+// proxy 函式中的第二個參數是課程需求而設定，實際上不一定需要
+app.use(
+  '/api',
+  proxy('http://react-ssr-api.herokuapp.com', {
+    proxyReqOptDecorator(proxyReqOpts) {
+      proxyReqOpts.headers['x-forwarded-host'] = 'localhost:3000';
+      return proxyReqOpts;
+    }
+  })
+);
 
 app.use(express.static('public'));
 
@@ -21,7 +34,7 @@ app.get('*', (req, res) => {
   Promise.all(promises).then(() => {
     // 當所有的 Promise 都 resolve 時才渲染 App
     res.send(renderer(req, serverStore));
-  })
+  });
 });
 
 app.listen(3000, () => {
